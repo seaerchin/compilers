b14cca22963af804aefdac636c189a84f79ee816
seaerchin <jer005@e.ntu.edu.sg>
2021-08-25 03:23:05 +0800

    feat(regex): parses to actual data structure

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,10 +4,15 @@ import Control.Monad (join)
 import Text.Parsec
 import Text.Parsec.String
 
+-- NOTE: there are actually 3 representations
+-- First, we tokenize the input text
+-- Next, we parse the input text to postfix notation. This introduces the AND operator (.)
+-- Lastly, we utilize shunting yard to get the AST from the postfix notation. This removes the LParen/RParen operators (A)
+
 -- Describes the set of regex expressions
 -- not supporting anymore for simplicity
 -- TODO: extend this to include choice potentially
-data Reg = RegEmpty | RegLiteral Char | RegOr Reg Reg | RegAnd Reg Reg | RegStar Reg deriving (Eq)
+data Reg = RegEmpty | RegLiteral Char | RegOr Reg Reg | RegAnd Reg Reg | RegStar Reg deriving (Eq, Show)
 
 data Token = Literal Char | Or | Star | LParen | RParen deriving (Eq, Show)
 
@@ -18,6 +23,11 @@ type Tokens = [Token]
 
 type ParsedToks = [Parsed]
 
+-- Curse the namespacing gods.
+data PostFix = PLiteral Char | POr | PStar | PAnd deriving (Eq, Show)
+
+type RPN = [PostFix]
+
 conv :: Char -> Token
 conv '*' = Parser.Star
 conv '|' = Parser.Or
@@ -25,18 +35,47 @@ conv '(' = LParen
 conv ')' = RParen
 conv c = Parser.Literal c
 
+-- Step 1: Tokenize input text
 -- convert a string into a list of tokens
 fromString :: String -> Tokens
 fromString = map conv
 
+-- Step 2: Adds a new (explicit) concat operator to the regex
+-- ie, ab -> a . b
+-- for all regexes of the form AB, where A and B are also regexes,
+-- we only add the . if A is a valid regex by itself.
+-- this implies that partial regexes (ie, A | (some part)) or (A .. some part <-- NO CLOSING
+-- both are not of the form AB and cannot be accepted.
+-- since our grammar is limited to * . | ( ), we can simplify.
+insertAnd :: Tokens -> ParsedToks
+insertAnd = f []
+  where
+    f output [] = output
+    -- only 1 element in the list
+    f output [tok] = f (output ++ [Tok tok]) []
+    -- at least 2 items in the list
+    f output (tok : next : tokens) =
+      let newTok = Tok tok
+          newOutput = output ++ [newTok]
+       in case tok of
+            -- operators awaiting application - add to output and skip
+            LParen -> f newOutput (next : tokens)
+            Parser.Or -> f newOutput (next : tokens)
+            -- valid regexes -> lookahead and decide if we need to add AND operator
+            _ -> case next of
+              Parser.Literal c -> f (newOutput ++ [Parser.And]) (next : tokens)
+              LParen -> f (newOutput ++ [Parser.And]) (next : tokens)
+              _ -> f newOutput (next : tokens)
+
+-- Step 3: Shunting yard and obtain AST
 -- an implementation of the shunting yard algorithm in haskell
 -- refer to: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
 -- note caveats:
 -- 1. shunting yard requires operator precedence/associativity
 -- because we support a limited grammar, this is explicitly ignored
 -- shunting yard has support for functions -> NOT IMPLEMENTED (since no functions)
-parseRegex :: ParsedToks -> ParsedToks
-parseRegex = f [] []
+parseRegex :: ParsedToks -> RPN
+parseRegex = map toPostFix . f [] []
   where
     -- f takes q stack input
     -- if there is no input and nothing on stack, we are done
@@ -68,31 +107,18 @@ parseRegex = f [] []
             Tok LParen -> (q, init stack)
             _ -> fixRParen (q ++ [topOp]) (init stack)
 
--- basically, this adds a new (explicit) concat operator to the regex
--- ie, ab -> a . b
--- for all regexes of the form AB, where A and B are also regexes,
--- we only add the . if A is a valid regex by itself.
--- this implies that partial regexes (ie, A | (some part)) or (A .. some part <-- NO CLOSING
--- both are not of the form AB and cannot be accepted.
--- since our grammar is limited to * . | ( ), we can simplify.
-insertAnd :: Tokens -> ParsedToks
-insertAnd = f []
-  where
-    f output [] = output
-    -- only 1 element in the list
-    f output [tok] = f (output ++ [Tok tok]) []
-    -- at least 2 items in the list
-    f output (tok : next : tokens) =
-      let newTok = Tok tok
-          newOutput = output ++ [newTok]
-       in case tok of
-            -- operators awaiting application - add to output and skip
-            LParen -> f newOutput (next : tokens)
-            Parser.Or -> f newOutput (next : tokens)
-            -- valid regexes -> lookahead and decide if we need to add AND operator
-            _ -> case next of
-              Parser.Literal c -> f (newOutput ++ [Parser.And]) (next : tokens)
-              _ -> f newOutput (next : tokens)
+parse :: String -> RPN
+parse = parseRegex . insertAnd . fromString
+
+toPostFix :: Parsed -> PostFix
+toPostFix t@(Tok (Literal c)) = PLiteral c
+toPostFix t@(Tok Or) = POr
+toPostFix t@(Tok Star) = PStar
+toPostFix And = PAnd
+-- all parenthesis should have been removed by the shunting yard.
+-- having a parenthesis indicates malformed input and we should crash.
+-- TODO: actually display a good error message.
+toPostFix paren = undefined
 
 -- io testing functions because lazy to write test suites since it's provably correct.
 testParse :: IO ()
--- src/Regex.hs
+++ src/Regex.hs
@@ -1,8 +1,8 @@
 module Regex where
 
-import Control.Applicative
 import Data.Char (toUpper)
-import Data.Functor.Identity
+import Data.List
+import Parser (Parsed (And, Tok), ParsedToks, PostFix (PAnd, PLiteral, POr, PStar), RPN, Reg (RegAnd, RegEmpty, RegLiteral, RegOr, RegStar), Token (Or, Star), parse)
 import Text.Parsec
 import Text.Parsec.Char (letter)
 
@@ -10,32 +10,56 @@ import Text.Parsec.Char (letter)
 -- this is a crude attempt to translate a regex into a nfa
 -- and subsequently, convert the nfa into a dfa
 
--- Describes the set of regex expressions
--- not supporting anymore for simplicity
--- TODO: extend this to include choice potentially
-data Reg = Empty | Literal Char | Or Reg Reg | And Reg Reg | Star Reg deriving (Eq)
-
 -- prints all the elements of the given regex
 literals :: Reg -> String
--- placeholder since we want to signal the presence of an empty string
-literals Regex.Empty = "~"
-literals (Literal s) = [s]
-literals (Or a b) = literals a ++ " | " ++ literals b
-literals (And a b) = literals a ++ " . " ++ literals b
-literals (Star reg) = literals reg ++ "*"
+-- placeholder since we want to signal the presence of an RegEmpty string
+literals RegEmpty = "~"
+literals (RegLiteral s) = [s]
+literals (RegOr a b) = literals a ++ " | " ++ literals b
+literals (RegAnd a b) = literals a ++ " . " ++ literals b
+literals (RegStar reg) = literals reg ++ "*"
 
 -- checks if the string matches the given regex
 matches :: Reg -> String -> Bool
-matches Regex.Empty s = null s
-matches (Literal c) s = [c] == s
+matches RegEmpty s = null s
+matches (RegLiteral c) s = [c] == s
 -- either the first regexp matches or second regexp matches or false
-matches (Or a b) s = matches a s || matches b s
+matches (RegOr a b) s = matches a s || matches b s
 -- generate all sublists from a b and check if any match
-matches (And a b) s = or [matches a s1 && matches b s2 | (s1, s2) <- splits s]
-matches (Star reg) s = matches Regex.Empty s || or [matches reg s1 && matches (Star reg) s2 | (s1, s2) <- frontSplits s]
+matches (RegAnd a b) s = or [matches a s1 && matches b s2 | (s1, s2) <- splits s]
+matches (RegStar reg) s = matches RegEmpty s || or [matches reg s1 && matches (Parser.RegStar reg) s2 | (s1, s2) <- frontSplits s]
 
 splits :: [a] -> [([a], [a])]
 splits s = [splitAt n s | n <- [0 .. length s]]
 
 frontSplits :: [a] -> [([a], [a])]
 frontSplits = tail . splits
+
+-- we support: * c | .
+-- because the only unary operator here is *
+-- we can simplify the structure of the parsed tokens
+-- it is either of: Expr Expr Op
+-- or Exp Op (this is explicitly *)
+-- hence, we just keep reading
+fromParsed :: RPN -> Reg
+fromParsed expr = head $ f [] expr
+  where
+    f stack [] = stack
+    f stack (op : rest) = case op of
+      PLiteral c -> f (stack ++ [RegLiteral c]) rest
+      POr ->
+        let lastElem = last stack
+            secondLast = (last . init) stack
+            rem = (init . init) stack
+            expr = RegOr lastElem secondLast
+         in f (rem ++ [expr]) rest
+      PStar ->
+        let expr = last stack in f (init stack ++ [RegStar expr]) rest
+      PAnd ->
+        let lastElem = last stack
+            secondLast = (last . init) stack
+            rem = (init . init) stack
+            expr = RegAnd lastElem secondLast
+         in f (rem ++ [expr]) rest
+
+test = print . fromParsed . Parser.parse
